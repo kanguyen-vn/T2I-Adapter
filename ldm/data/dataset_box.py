661c10495c9ea59498d7dd9654af306c60c0e22e
Kiet Nguyen <kanguyen.vn@gmail.com>
2023-04-24 18:44:52 -0400

    debug box dataset

--- ldm/data/dataset_box.py
+++ ldm/data/dataset_box.py
@@ -1,11 +1,14 @@
 import json
 import cv2
 import os
-from basicsr.utils import img2tensor 
-import torch 
+from basicsr.utils import img2tensor
+import torch
+
 
 class dataset_coco_box:
-    def __init__(self, path_json, root_path_im, root_path_box, image_size,text_encoder):
+    def __init__(
+        self, path_json, root_path_im, root_path_box, image_size, text_encoder
+    ):
         super(dataset_coco_box, self).__init__()
         with open(path_json, "r", encoding="utf-8") as fp:
             data = json.load(fp)
@@ -14,19 +17,26 @@ class dataset_coco_box:
         self.root_path_im = root_path_im
         self.root_path_box = root_path_box
         for file in data:
-            name = "%012d.png" % file["image_id"]
-            box_cords = [i['bbox'] for i in file['all_bbox']]
-            box_cords = [ [i[0],i[1],i[0]+i[2],i[1]+i[3] ] for i in box_cords]
-            labels = [i['label']['category'] for i in file['all_bbox']]
+            name = "%012d.jpg" % file["image_id"]
+            box_cords = [i["bbox"] for i in file["all_bbox"]]
+            box_cords = [[i[0], i[1], i[0] + i[2], i[1] + i[3]] for i in box_cords]
+            labels = [i["label"]["category"] for i in file["all_bbox"]]
             box_cords_tensor = torch.tensor(box_cords)
-            self.files.append({"name": name, "sentence": file["caption"], "boxes": box_cords_tensor, "labels":labels})
+            self.files.append(
+                {
+                    "name": name,
+                    "sentence": file["caption"],
+                    "boxes": box_cords_tensor,
+                    "labels": labels,
+                }
+            )
         self.text_encoder = text_encoder
 
     def __getitem__(self, idx):
         file = self.files[idx]
         name = file["name"]
-        
-        im = cv2.imread(os.path.join(self.root_path_im, name.replace(".png", ".jpg")))
+
+        im = cv2.imread(os.path.join(self.root_path_im, name))
         im = cv2.resize(im, (512, 512))
         im = img2tensor(im, bgr2rgb=True, float32=True) / 255.0
 
@@ -36,13 +46,13 @@ class dataset_coco_box:
             img2tensor(mask, bgr2rgb=True, float32=True) / 255.0
         )  # [0].unsqueeze(0)#/255.
         sentence = file["sentence"]
-        boxes = file['boxes']
+        boxes = file["boxes"]
         labels = []
-        for label in file['labels']:
+        for label in file["labels"]:
             encoded_label = self.text_encoder(label)
             labels.append(encoded_label)
         labels = torch.stack(labels)
-        
+
         return {
             "im": im,
             "mask": mask,
